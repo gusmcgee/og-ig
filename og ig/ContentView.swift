d5fd4fae2951166abda0cc91ca18a74df7ed9e98
armcgee2 <augustus.mcgee@icloud.com>
2026-07-14 22:26:35 -0400

    Revert back to WK tooling and first pass config

--- og ig/ContentView.swift
+++ og ig/ContentView.swift
@@ -8,23 +8,52 @@
 import SwiftUI
 import WebKit
 
-struct WebView: UIViewRepresentable {
+struct AppView: UIViewRepresentable {
     let url: URL
 
     func makeUIView(context: Context) -> WKWebView {
-        let wv = WKWebView()
-        wv.isInspectable = true
-        return wv
+        let scriptURL = Bundle.main.url(forResource: "script", withExtension: "js")!
+        
+        let jsStr = try! String(contentsOf: scriptURL, encoding: .utf8)
+        
+        let script = WKUserScript(
+            source: jsStr,
+            injectionTime: .atDocumentStart,
+            forMainFrameOnly: false
+        )
+        
+        let config = WKWebViewConfiguration()
+
+        config.userContentController.addUserScript(script)
+        
+        config.allowsInlineMediaPlayback = true
+        config.mediaTypesRequiringUserActionForPlayback = []
+        config.suppressesIncrementalRendering = true
+
+        let webView = WKWebView(frame: .zero, configuration: config)
+        webView.load(URLRequest(url: url))
+        webView.isInspectable = true
+        
+        webView.scrollView.bounces = true
+        webView.scrollView.alwaysBounceVertical = true
+        webView.scrollView.alwaysBounceHorizontal = false
+        
+//        webView.isOpaque = false                  // stop the white flash before paint
+//        webView.backgroundColor = .black
+//        webView.scrollView.showsVerticalScrollIndicator = false
+        webView.allowsBackForwardNavigationGestures = true   // edge-swipe back
+        webView.allowsLinkPreview = false
+        
+        return webView
     }
 
-    func updateUIView(_ wv: WKWebView, context: Context) {
-        wv.load(URLRequest(url: url))
-    }
+    func updateUIView(_ webView: WKWebView, context: Context) {}
 }
 
 struct ContentView: View {
+    private let url = URL(string: "https://www.instagram.com")!
+
     var body: some View {
-        WebView(url: URL(string: "https://www.instagram.com")!)
-            .ignoresSafeArea(edges: .bottom)
+        AppView(url: url)
     }
 }
